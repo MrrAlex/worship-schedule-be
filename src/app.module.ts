8f521de5b132dd6772bd23eb17ddde07ff7587ab
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-07-31 22:33:07 +0300

    Build fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { DbModule } from './db/db.module';
 import { CourseController } from './controllers/course.controller';
 import { StudyModuleController } from './controllers/study-module.controller';
 import { LessonsController } from './controllers/lessons.controller';
 import { AnswersController } from './controllers/answers.controller';
-import { PDFModule } from '@t00nday/nestjs-pdf';
 import { PdfGeneratorService } from './services/pdf-generator.service';
 
 @Module({
