a189887d5e72eb6e04cdc0976fcccd961d9b9c7c
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-08-01 21:31:32 +0300

    Add another cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:8000'],
+    origin: [
+      'http://localhost:4200',
+      'http://finuchenie.online',
+      'http://localhost:8000',
+    ],
     credentials: true,
   });
   await app.listen(3000);
