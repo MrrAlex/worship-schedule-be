0ab9f3b16f806ce910bca4c63dceb88dc1be4d38
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-11-02 21:16:40 +0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
       'http://localhost:4200',
       'http://finuchenie.online',
       'http://localhost:8000',
+      'https://fc-fe.vercel.app',
     ],
     credentials: true,
   });
